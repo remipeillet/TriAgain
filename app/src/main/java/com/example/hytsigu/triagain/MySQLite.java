d93c991be80d8aa223d55ac088ea0a817402b2d7
charlotteDavid <charlotte.david96@gmail.com>
2017-12-19 15:59:54 +0100

    class MySQLite

--- app/src/main/java/com/example/hytsigu/triagain/MySQLite.java
+++ app/src/main/java/com/example/hytsigu/triagain/MySQLite.java
@@ -4,5 +4,38 @@ package com.example.hytsigu.triagain;
  * Created by Utilisateur on 19/12/2017.
  */
 
-public class MySQLite {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class MySQLite extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "db.sqlite";
+    private static final int DATABASE_VERSION = 1;
+    private static MySQLite sInstance;
+
+    public static synchronized MySQLite getInstance(Context context) {
+        if (sInstance == null) { sInstance = new MySQLite(context); }
+        return sInstance;
+    }
+
+    private MySQLite(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        // Création de la base de données
+        // on exécute ici les requêtes de création des tables
+        sqLiteDatabase.execSQL(DechetDAO.CREATE_TABLE_DECHET); // création table "dechet"
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i2) {
+        // Mise à jour de la base de données
+        // méthode appelée sur incrémentation de DATABASE_VERSION
+        // on peut faire ce qu'on veut ici, comme recréer la base :
+       // onCreate(sqLiteDatabase);
+    }
+
 }
