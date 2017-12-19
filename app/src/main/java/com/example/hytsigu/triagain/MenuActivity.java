9fad88c59f87bf32bf9b8d927c9ba818e77c8cd9
Rémi <peillet.e1501234@etud.univ-ubs.fr>
2017-12-19 14:03:47 +0100

    Appel de l'activité SerieDechet au clique sur le bouton

--- app/src/main/java/com/example/hytsigu/triagain/MenuActivity.java
+++ app/src/main/java/com/example/hytsigu/triagain/MenuActivity.java
@@ -1,13 +1,30 @@
 package com.example.hytsigu.triagain;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.view.View.OnClickListener;
 
 public class MenuActivity extends AppCompatActivity {
+
+    ImageButton startButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+        this.startButton  = (ImageButton) findViewById(R.id.startButton);
+        this.startButton.setOnClickListener(startListener);
+
     }
+
+    private OnClickListener startListener = new OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent myIntent = new Intent(getApplicationContext(), SerieDechetActivity.class);
+            startActivityForResult(myIntent, 1);
+        }
+    };
 }
---
+++ app/src/main/java/com/example/hytsigu/triagain/SerieDechetActivity.java
@@ -0,0 +1,13 @@
+package com.example.hytsigu.triagain;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SerieDechetActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_serie_dechet);
+    }
+}
