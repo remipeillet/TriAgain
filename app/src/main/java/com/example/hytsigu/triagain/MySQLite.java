a31e3c483d74181294eefc6eb2acc002813a51d6
Rémi <peillet.e1501234@etud.univ-ubs.fr>
2017-12-19 17:13:01 +0100

    Controle serie Gestion database

--- app/src/main/java/com/example/hytsigu/triagain/MySQLite.java
+++ app/src/main/java/com/example/hytsigu/triagain/MySQLite.java
@@ -8,26 +8,52 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 public class MySQLite extends SQLiteOpenHelper {
 
-    private static final String DATABASE_NAME = "db.sqlite";
+    private static final String DATABASE_NAME = "tri_again.db";
     private static final int DATABASE_VERSION = 1;
     private static MySQLite sInstance;
+    private Context context;
 
     public static synchronized MySQLite getInstance(Context context) {
+        System.out.println(">>>>>>>>>>getInstance");
         if (sInstance == null) { sInstance = new MySQLite(context); }
         return sInstance;
     }
 
     private MySQLite(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        System.out.println(">>>>>>>>>>constructeur");
+        this.context = context;
+        this.onCreate(this.getWritableDatabase());
     }
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
         // Création de la base de données
         // on exécute ici les requêtes de création des tables
-        sqLiteDatabase.execSQL(DechetDAO.CREATE_TABLE_DECHET); // création table "dechet"
+        try {
+            String requete = "";
+            InputStream ips = context.getResources().getAssets().open("tri_again_database.sql");
+            InputStreamReader ipsr=new InputStreamReader(ips);
+            BufferedReader br=new BufferedReader(ipsr);
+            String ligne;
+            while ((ligne=br.readLine())!=null){
+                //System.out.println(ligne);
+                requete+=ligne;
+            }
+            System.out.println(">>>>>>>>>>"+requete);
+            br.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        //sqLiteDatabase.execSQL(DechetDAO.CREATE_TABLE_DECHET);// création table "dechet"
     }
 
     @Override
